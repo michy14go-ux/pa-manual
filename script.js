3262883cd4a72d3b5052929035aa3a4c532e958f
michy14go-ux <michy14go@gmail.com>
2026-07-14 18:39:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,69 @@
+const menuElement = document.getElementById("menu");
+const pageContent = document.getElementById("pageContent");
 
-console.log('PA Manual Loaded');
+async function loadMenu() {
+
+    const response = await fetch("data/menu.json");
+    const data = await response.json();
+
+    menuElement.innerHTML = "";
+
+    createMenu(data.menu, menuElement);
+
+}
+
+function createMenu(items, parent) {
+
+    const ul = document.createElement("ul");
+
+    items.forEach(item => {
+
+        const li = document.createElement("li");
+
+        if(item.page){
+
+            li.textContent = item.title;
+
+            li.onclick = () => {
+
+                loadPage(item.page);
+
+            };
+
+        }
+
+        if(item.children){
+
+            const title = document.createElement("div");
+
+            title.textContent = "▼ " + item.title;
+
+            title.className = "folder";
+
+            li.appendChild(title);
+
+            createMenu(item.children, li);
+
+        }
+
+        ul.appendChild(li);
+
+    });
+
+    parent.appendChild(ul);
+
+}
+
+async function loadPage(page){
+
+    const response = await fetch("pages/" + page + ".html");
+
+    const html = await response.text();
+
+    pageContent.innerHTML = html;
+
+}
+
+loadMenu();
+
+loadPage("home");
